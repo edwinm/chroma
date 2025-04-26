0701456a7a93e5edb2095c534bf476b1aba514c0
Edwin Martin <edwin@bitstorm.org>
2025-04-26 09:36:30 +0200

    Use TFT_eSPI with ST7735

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,16 @@
-#include <Adafruit_ST7735.h>
+#include <Arduino.h>
+#include <HardwareSerial.h>
+#include <TFT_eSPI.h>
 
-// Define pins
-#define TFT_CS    5
-#define TFT_RST   4
-#define TFT_DC    2
-
-// Create an instance of the ST7735 class
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+TFT_eSPI tft = TFT_eSPI();
 
 void setup() {
-  // Initialize serial communication
-  Serial.begin(115200);
+  Serial.begin(9600);
+  Serial.println("Starting...");
 
-  // Initialize the display
-  tft.initR(INITR_BLACKTAB); // For 1.8" displays, use INITR_144GREENTAB for 1.44"
-  tft.setRotation(2); // Adjust orientation
+  tft.init();
+  tft.fillScreen(TFT_BLUE);
 
-  // Clear the screen with a color
-  tft.fillScreen(ST77XX_BLACK);
-
-  // Display text
-  tft.setTextColor(ST77XX_WHITE);
-  tft.setTextSize(1);
-  tft.setCursor(30, 30);
-  tft.println("Hello, ST7735!");
 }
 
 void loop() {
